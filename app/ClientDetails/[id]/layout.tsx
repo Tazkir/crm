ae0e04af2576d60a88e27025e2d4e0887a4c2047
tazkir <tazkirfurqan77@gmail.com>
2023-06-26 12:28:55 +0800

    update unit test
    Signed-off-by: tazkir <tazkirfurqan77@gmail.com>

--- app/ClientDetails/[id]/layout.tsx
+++ app/ClientDetails/[id]/layout.tsx
@@ -12,7 +12,7 @@ function RootLayout() {
     <div className="flex flex-col min-h-screen justify-center items-center gap-5">
       <div className="bg-slate-400/20 rounded-2xl p-2">
         <Link href="/" className="text-lg font-bold">
-          &lt; back
+          back
         </Link>
         <h1 className="text-3xl">Client Details</h1>
       </div>
--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,38 +4,42 @@ describe('Adding Client', () => {
 
     cy.wait(3000);
 
-    cy.get('button').contains('+ New Client').click();
+    cy.get('button').contains('+ New Client').wait(2000).click();
 
-    cy.get('.name').type('Amin').should('have.value', 'Amin');
+    cy.get('.name').type('Amin').should('have.value', 'Amin').wait(2000);
 
     cy.get('.contact')
       .type('012-3242-232')
-      .should('have.value', '012-3242-232');
+      .should('have.value', '012-3242-232')
+      .wait(2000);
 
-    cy.get('.org').type('Harry Inc').should('have.value', 'Harry Inc');
+    cy.get('.org')
+      .type('Harry Inc')
+      .should('have.value', 'Harry Inc')
+      .wait(1000);
 
-    cy.get('.assigned').type('Jones').should('have.value', 'Jones');
+    cy.get('.assigned').type('Jones').should('have.value', 'Jones').wait(1000);
 
-    cy.get('button').contains('Save changes').click();
+    cy.get('button').contains('Save changes').wait(3000).click();
 
-    cy.get('#close-button').click();
+    cy.get('#close-button').wait(1000).click();
 
-    cy.get('#refresh').click();
+    cy.get('#refresh').wait(2000).click();
 
-    cy.get('table').get('#action').should('be.visible').click();
+    cy.get('table').get('#action').should('be.visible').wait(1000).click();
 
-    cy.get('a').contains('Client Detail').click();
+    cy.get('a').contains('Client Detail').wait(3000).click();
 
-    cy.get('a').contains('Back').click();
+    cy.get('a').contains('back').wait(3000).click();
 
-    cy.get('table').get('#action').should('be.visible').click();
+    cy.get('table').get('#action').should('be.visible').wait(1000).click();
 
-    cy.get('.inactive').contains('Inactive').click();
+    cy.get('.inactive').contains('Inactive').wait(3000).click();
 
-    cy.get('a').contains('Client Detail').click();
+    cy.get('a').contains('Client Detail').wait(3000).click();
 
-    cy.get('a').contains('Back').click();
+    cy.get('a').contains('back').wait(3000).click();
 
-    cy.get('#refresh').click();
+    cy.get('#refresh').click().wait(3000);
   });
 });
